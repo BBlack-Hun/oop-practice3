efd37daa8728574816150ecc68fdaad1441d6500
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-11-05 00:21:45 +0900

    음식점에서 음식 주문하는 과정 구현하기 - 마무리

--- src/main/java/com/example/ooppractice3/MenuItem.java
+++ src/main/java/com/example/ooppractice3/MenuItem.java
@@ -1,5 +1,7 @@
 package com.example.ooppractice3;
 
+import java.util.Objects;
+
 public class MenuItem {
     private final String name;
     private final int price;
@@ -10,6 +12,10 @@ public class MenuItem {
         this.price = price;
     }
 
+    public boolean matches(String name) {
+        return this.name.equals(name);
+    }
+
     public String getName() {
         return name;
     }
@@ -17,4 +23,18 @@ public class MenuItem {
     public int getPrice() {
         return price;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuItem menuItem = (MenuItem) o;
+        return price == menuItem.price && Objects.equals(name, menuItem.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
 }
